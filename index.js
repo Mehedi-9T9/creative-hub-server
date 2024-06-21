f496cb6388b43f3815290e34062c1d06d5848fc4
Mehedi-9T9 <mehedi.hasan9t9@gmail.com>
2024-06-21 10:33:14 +0600

    authentication related apis create successful

--- index.js
+++ index.js
@@ -29,12 +29,47 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+        const usersCollection = client.db("creative-hub").collection("users");
+
+        app.post("/rejisterUser", async (req, res) => {
+            const userInfo = req.body
+            // const email = userInfo.email
+            const filter = { email: userInfo?.email }
+            const search = await usersCollection.findOne(filter)
+
+            if (!search) {
+                const result = await usersCollection.insertOne(userInfo)
+                return res.send(result)
+            }
+            res.send({ message: "Authentication Fail" })
+
+        })
+
+        //Login users
+        app.post("/loginData", async (req, res) => {
+            const userInfo = req.body
+            const filter = { email: userInfo.email, password: userInfo.password }
+            const search = await usersCollection.findOne(filter)
+            if (search) {
+                return res.send({ message: true })
+            } else { return res.send({ message: false }) }
+        })
+
+        app.get('/usersData', async (req, res) => {
+            const email = req.query.email
+            const filter = { email: email }
+            const result = await usersCollection.findOne(filter)
+            res.send(result)
+        })
+
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.log);
